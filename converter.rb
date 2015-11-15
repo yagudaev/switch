d77044b87e9cf1a3542fd0d1b5ac8a5a7725ed92
Michael Yagudaev <michael@yagudaev.com>
2015-11-14 18:16:21 -0800

    add support for multiple commands

--- converter.rb
+++ converter.rb
@@ -18,9 +18,21 @@ require './json2csv'
 require './cloud_sync'
 
 def main
-  csv_file = Json2Csv.new('./data/*').convert
-  client = CloudSync.new
-  client.upload_to_drive(csv_file)
+  command = ARGV[0]
+
+  case command
+  when "json2csv"
+    csv_file = Json2Csv.new('./data/*').convert
+    client = CloudSync.new
+    client.upload_to_drive(csv_file)
+  when "csv2json"
+    puts "to be implemented!"
+  else
+    puts "Unknown option!"
+    puts "Please use either"
+    puts "  json2csv - converts multiple json files to csv files and uplaods them to google drive"
+    puts "  csv2json - converts a single csv file to multiple json files"
+  end
 end
 
 main
